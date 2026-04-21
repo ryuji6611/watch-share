681fb22985bcc0369bfe963ae2eb09b201982db9
ryuji <ryuji1023au@icloud.com>
2026-04-21 21:40:51 +0900

    Fix Supabase config

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 ﻿window.WATCHSHARE_SUPABASE_URL = "https://ndfwyruhgkdybnmxgkjw.supabase.co";
 window.WATCHSHARE_SUPABASE_ANON_KEY = "sb_publishable_grDkpmua09ejq7viyjqmTg_ZKgMc7oE";
-window.WATCHSHARE_TMDB_API_KEY = "YOUR_TMDB_API_KEY";
+window.WATCHSHARE_TMDB_API_KEY = "4e2d33eacadbe1c30ca1743864082225";
